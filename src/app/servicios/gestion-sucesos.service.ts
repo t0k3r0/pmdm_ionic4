9e8a0b3d7d789e35b863f3eb9198ce6ae3f8e662
Joseba Sanchez <josebsanchez@birt.eus>
2022-05-28 21:12:59 +0200

    initial commit

--- src/app/servicios/gestion-sucesos.service.ts
+++ src/app/servicios/gestion-sucesos.service.ts
@@ -1,9 +1,14 @@
+// import { ISucesos } from './gestion-sucesos.service';
+/* eslint-disable space-before-function-paren */
+/* eslint-disable @typescript-eslint/semi */
+/* eslint-disable eqeqeq */
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+import { GestionStorageService } from './gestion-storage.service';
 /* eslint-disable prefer-const */
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 // import { IpcNetConnectOpts } from 'net';
-
 
 export interface ISucesos {
   id: string;
@@ -11,6 +16,7 @@ export interface ISucesos {
   town: string;
 
 }
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,11 +24,19 @@ export class GestionSucesosService {
   // id: string;
   // source: string;
   // town: string;
-  private sucesos: ISucesos[]=[];
+  private sucesos: ISucesos[] = [];
 
-  constructor(private leerFichero: HttpClient) {
+  constructor(private leerFichero: HttpClient, private gestionAlmacen: GestionStorageService) {
     console.log('2.1');
     this.getSucesosFichero();
+    let datosPromesa: Promise<ISucesos[]> = gestionAlmacen.getObject('personas');
+
+
+    datosPromesa.then(datos => {
+      this.sucesos.push(...datos);
+
+    });
+
   }
 
 
@@ -30,6 +44,7 @@ export class GestionSucesosService {
     let datosFichero: Observable<ISucesos[]>;
     datosFichero = this.leerFichero.get<ISucesos[]>('/assets/datos/sucesos.json');
     datosFichero.subscribe(datos => (this.sucesos.push(...datos)));
+    this.gestionAlmacen.setObject('sucesos', this.sucesos);
     console.log('2.2');
 
   }
@@ -38,9 +53,19 @@ export class GestionSucesosService {
     console.log('2.3');
     return this.sucesos;
   }
+
+  modificarSuceso(id: string, source: string, town: string) {
+
+    let sucesoEncontrado: ISucesos = this.sucesos.find(function (cadaSuceso) { return cadaSuceso.id == id });
+    let indice: number = this.sucesos.indexOf(sucesoEncontrado);
+    this.sucesos[indice].source = source;
+    this.sucesos[indice].town = town;
+    this.gestionAlmacen.setObject('sucesos', this.sucesos);
+    console.log('2.2');
+  }
   // insertarSuceso(id: string, source: string, town: string) {
 
-    // eslint-disable-next-line prefer-const
+  // eslint-disable-next-line prefer-const
   //   let nuevoSuceso: ISucesos = {
   //     id: this.id,
   //     source: this.source,
@@ -54,14 +79,14 @@ export class GestionSucesosService {
   // }
 
 
-  // borrarSuceso(id: string) {
+  borrarSuceso(id: string) {
     // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
     // eslint-disable-next-line space-before-function-paren
     // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
     // eslint-disable-next-line prefer-const
     // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
-    // let sucesoEncontrado: ISucesos = (function (ISucesos) { return ISucesos.id === id});
+    // let sucesoEncontrado: ISucesos = (function (sucesos) { return sucesos.id === id});
     // let indice: number = this.sucesos.indexOf(sucesoEncontrado);
 
-  // }
+  }
 }
